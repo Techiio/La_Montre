7261d388df7e41aecfead0fa3c62496c18b2167e
fsasaki <45539473+fsasaki10@users.noreply.github.com>
2021-12-13 10:46:23 +0100

    ajout texte déroulant

--- user-gest-admin_faq-contact.php
+++ user-gest-admin_faq-contact.php
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>La Montre - FAQ</title>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Montre - FAQ</title>
 
-        <!-- font awesome cdn link  -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-        <!-- custom css file link  -->
-        <link rel="stylesheet" href="css/style.css">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
 
-    </head>
+</head>
 <body>
 
+</body>
 <!-- header section starts  -->
 
 <header class="header">
 
-    <a class="logo">
+    <a href="#" class="logo">
         <img src="images/EkoS.png" alt="">
     </a>
 
     <nav class="navbar">
-        <a href="user-gest-admin_menu.php">Mon Menu</a>
-        <a href="user-gest-admin_ma-journee.php">Ma Journée</a>
-        <a href="user-gest-admin_statistiques.php">Mes Stats</a>
-        <a href="user-gest-admin_conseils.php">Mes Conseils</a>
+        <a href="#User">[USER.NAME.TBD]</a>
     </nav>
 
-    <a href="visiteur_accueil.php" class="logo">
+    <a href="#" class="logo">
         <img src="images/LaMontreS.png" alt="">
-        <h3>Déconnexion</h3>
     </a>
 
 </header>
@@ -41,9 +41,9 @@
 <!-- header section ends -->
 
 <!-- contact section starts  -->
-<section class="content">
-    <h1 class="heading"> . </h1>
-</section>
+<div class="content">
+    <h1 class="heading"> <span>contact</span> us </h1>
+</div>
 
 <section class="contact" id="contact">
 
@@ -51,10 +51,17 @@
 
     <div class="row">
 
-        <div class="faq">
-            <h3>Foire aux questions</h3>
-            <div>
+        <div class="div1">
+            <h3 > Foire aux questions</h3>
+            <p class="texte">
+                Cette page montre l'emboîtement des balises "div". La feuille de style définit une hauteur de 200 pixels pour la boîte "div2" avec adjonction de la spécification <b>overflow : auto</b>. On peut alors faire défiler le texte qu'elle contient par la barre de défilement qui apparaît sur la droite. La largeur de la boîte "div2" n'est pas spécifiée. Elle prend toute la largeur disponible compte tenu des marges. <br/>Les boîtes internes héritent des propriétés des boîtes qui les englobent : ce paragraphe de style "texte0" est dans la boîte "div2", elle même contenue dans la boîte "div1". Il hérite de la police de caractères de "div1". Ses marges sont annulées par son style "texte0"
+            </p>
 
+            <p class="texte1">
+                Ce paragraphe est dans la boîte "div2" et répond au style "texte1". Ce style modifie la couleur de la police de caractères héritée de "div1", annule les marges, puis fixe la marge haute à 20px
+            </p>
+
+        </div>
 
         <form action="">
             <h3>get in touch</h3>
@@ -66,6 +73,10 @@
                 <span class="fas fa-envelope"></span>
                 <input type="email" placeholder="email">
             </div>
+            <div class="inputBox">
+                <span class="fas fa-phone"></span>
+                <input type="number" placeholder="number">
+            </div>
             <input type="submit" value="contact now" class="btn">
         </form>
 
@@ -73,17 +84,53 @@
 
 </section>
 
+
+
 <!-- contact section ends -->
+
+</html>
+
+<!-- custom css file link  -->
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a class="logo">
+        <img src="images/EkoS.png" alt="">
+    </a>
+
+    <nav class="navbar">
+        <a href="user-gest-admin_menu.html">Mon Menu</a>
+        <a href="user-gest-admin_ma-journee.html">Ma Journée</a>
+        <a href="user-gest-admin_statistiques.html">Mes Stats</a>
+        <a href="user-gest-admin_conseils.html">Mes Conseils</a>
+        <a href="user-gest-admin_menu.php">Mon Menu</a>
+        <a href="user-gest-admin_ma-journee.php">Ma Journée</a>
+        <a href="user-gest-admin_statistiques.php">Mes Stats</a>
+        <a href="user-gest-admin_conseils.php">Mes Conseils</a>
+    </nav>
+
+    <a href="index.html" class="logo">
+        <a href="visiteur_accueil.php" class="logo">
+            <img src="images/LaMontreS.png" alt="">
+            <h3>Déconnexion</h3>
+        </a>
+
+</header>
+
+<!-- header section ends -->
+
 
 <section class="footer">
 
     <div class="links">
-        <a href="visiteur_CGU.php">CGU</a>
+        <a href="visiteur_CGU.html">CGU</a>
     </div>
 
 </section>
 
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
-
-</html>
