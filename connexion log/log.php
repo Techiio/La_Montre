a4305d5680eaa7a3282cb84cfc0b44aef5b6e447
fsasaki <45539473+fsasaki10@users.noreply.github.com>
2022-01-04 14:45:57 +0100

    Droit admin

--- connexion log/log.php
+++ connexion log/log.php
@@ -26,7 +26,21 @@ if (!empty($_POST["Identifiant"]) && !empty($_POST["Mdp"])) {
     $req = $db->prepare('SELECT * FROM connexion WHERE Identifiant=?');
     $req->execute(array($c_mail));
     while ($user = $req->fetch()) {
-        if ($c_mdp == $user['Mdp']) {
+
+
+        if ( $c_mail == 'jesapel' && $c_mdp == 'groot' ) {
+        $error = 0;
+
+        setcookie('pseudo', $user['Identifiant'], time()+364*24*3600, '/', null, false, true);
+        setcookie('codeS', $user['CodeStatut'], time()+364*24*3600, '/', null, false, true);
+
+        $_SESSION['connect'] = 1;
+
+        header('location: ../admin_screen-gestion.php');
+
+    }
+
+        elseif ($c_mdp == $user['Mdp']) {
             $error = 0;
 
             setcookie('pseudo', $user['Identifiant'], time()+364*24*3600, '/', null, false, true);
