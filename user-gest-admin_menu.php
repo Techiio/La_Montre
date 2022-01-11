2378ce69837bcf545440729d671adf1adb1a504f
fsasaki <45539473+fsasaki10@users.noreply.github.com>
2022-01-11 21:09:26 +0100

    Redirection

--- user-gest-admin_menu.php
+++ user-gest-admin_menu.php
@@ -7,10 +7,7 @@
     <title>Home Menu</title>
 
     <!-- font awesome cdn link  -->
-    <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 
     <!-- custom css file link  -->
     <link rel="stylesheet" href="css/style.css">
@@ -89,8 +86,6 @@
         </a>' ;
         }
         ?>
-
-
 
     </div>
 
