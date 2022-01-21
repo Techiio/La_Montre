09c3cce33aebb11b254a41290c5355b8ad56d37f
Benoit <benoit.thomas@eleve.isep.fr>
2022-01-21 15:55:12 +0100

    Nom des pages header

--- user-gest-admin/user-gest-admin_conseils.php
+++ user-gest-admin/user-gest-admin_conseils.php
@@ -11,7 +11,7 @@ if(!isset($_SESSION['statut'])) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Visiteur Mes Conseils</title>
+        <title>Mes Conseils</title>
 
         <!-- font awesome cdn link  -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
--- user-gest-admin/user-gest-admin_menu.php
+++ user-gest-admin/user-gest-admin_menu.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['statut'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Menu</title>
+    <title>Menu</title>
 
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
