0f3cafc4fcf964f89440abaa1a21f5a1653f27c3
Benoit <benoit.thomas@eleve.isep.fr>
2022-01-25 12:34:44 +0100

    ------- version finale ------- Adaptée a localhost (sans mail) -----------------------------

--- load/config_PDO.php
+++ load/config_PDO.php
@@ -2,7 +2,7 @@
     //Connexion à la BDD
     try
     {
-        $bdd = new PDO('mysql:host=localhost;dbname=bdd;charset=utf8','root', 'CVZVb3vnpXrC');
+        $bdd = new PDO('mysql:host=localhost;dbname=bdd;charset=utf8','root', '');
     }catch(Exception $e)
     {
         die('Erreur'.$e->getMessage());
