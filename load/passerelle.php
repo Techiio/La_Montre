dd390803626c9904398e31dca8e83c4fcfd3b3ec
Benoit <benoit.thomas@eleve.isep.fr>
2022-06-15 12:03:37 +0200

    maj passerelle.php

--- load/passerelle.php
+++ load/passerelle.php
@@ -4,17 +4,11 @@ require_once("config_PDO.php");
 $datalamontre = "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=6742";
 $data = file_get_contents($datalamontre);
 
-
-echo "Raw Data:<br />";
-echo($data);
-
 $data_tab = str_split($data,33);
-echo "Tabular Data:<br />";
 
 $num = [];
 $numverif = 0;
 $iteration = 1;
-$compteur = 1;
 $bpm = 0;
 $db = 0;
 $No2 = 0;
@@ -24,7 +18,6 @@ $sql = 'DELETE FROM `donneesmontre`';
 $req = $bdd->query($sql);
 
 for($i=0, $size=count($data_tab); $i<$size; $i++){
-    echo "Trame $i: $data_tab[$i]";
 
     $trame = $data_tab[$i];
     // décodage avec des substring
@@ -35,7 +28,6 @@ for($i=0, $size=count($data_tab); $i<$size; $i++){
     // décodage avec sscanf
     list($t, $codeproduit, $r, $type, $n, $valeur, $boucle, $x, $year, $month, $day, $hour, $min, $sec) =
         sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
-    echo("<br />$t,$codeproduit,$r,$type,$n,$valeur,$boucle,$x,$year,$month,$day,$hour,$min,$sec<br /><br/>");
 
     $date = $year . '-' . $month . '-'. $day;
     $time = $hour . ':' . $min . ':' . $sec;
@@ -51,9 +43,8 @@ for($i=0, $size=count($data_tab); $i<$size; $i++){
     }
     if($type == 4){
         $bpm = $valeur;
-        $requete = $bdd->prepare('INSERT INTO donneesmontre(Date, Heure, Bpm, dB, No2, DegréCelsius, CodeProduit, compteur) VALUES(?, ?, ?, ?, ?, ?, ?,?)');
-        $requete->execute(array($date, $time, $bpm, $db, $No2, $degCel, $codeproduit, $compteur));
-        $compteur +=1;
+        $requete = $bdd->prepare('INSERT INTO donneesmontre(Date, Heure, Bpm, dB, No2, DegréCelsius, CodeProduit) VALUES(?, ?, ?, ?, ?, ?,?)');
+        $requete->execute(array($date, $time, $bpm, $db, $No2, $degCel, $codeproduit));
         $iteration =0;
     }
     $iteration +=1;
